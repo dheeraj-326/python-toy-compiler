4b411cb168a269455bd9e87d79c6b3cbd5db3118
dheeraj-outwin-at-271334197815 <dheeraj.326@gmail.com>
2021-08-16 17:07:30 +0530

    Completing exercise 2

--- src/main.py
+++ src/main.py
@@ -3,5 +3,14 @@ from constants.constants import OPERATOR
 from expressions.expressions import BinaryExp, NumericConstant
 
 
-binary_expression = BinaryExp(NumericConstant(5), NumericConstant(10), OPERATOR.MUL)
-print(binary_expression.evaluate(None))
+def step1():
+    binary_expression = BinaryExp(NumericConstant(5), NumericConstant(10), OPERATOR.MUL)
+    print(binary_expression.evaluate(None))
+
+def step2():
+    expression = BinaryExp(NumericConstant(5), BinaryExp(NumericConstant(30), NumericConstant(50), OPERATOR.PLUS), OPERATOR.PLUS)
+    print(expression.evaluate(None))
+
+if __name__ == "__main__":
+    # step1()
+    step2()
